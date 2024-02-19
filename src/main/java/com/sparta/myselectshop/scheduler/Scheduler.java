59492ed0471190b4229fd56244977109f5b194e2
kimpangya <nam74415133@gmail.com>
2024-02-19 21:33:18 +0900

    Scheduler 기능 구현

--- src/main/java/com/sparta/myselectshop/scheduler/Scheduler.java
+++ src/main/java/com/sparta/myselectshop/scheduler/Scheduler.java
@@ -36,6 +36,8 @@ public class Scheduler {
             List<ItemDto> itemDtoList = naverApiService.searchItems(title);
 
             if (itemDtoList.size() > 0) {
+                //이거 왜 0이냐면 itemDtoList에서 관심 상품 이름으로 검색했는데
+                //0번째거를 가져와야 제일 일치하는 상품일 거 아님? 그런거임
                 ItemDto itemDto = itemDtoList.get(0);
                 // i 번째 관심 상품 정보를 업데이트합니다.
                 Long id = product.getId();
