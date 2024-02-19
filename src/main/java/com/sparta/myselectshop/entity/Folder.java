f94486a76c60caac20dc3b90bbb9fb7d7472a1c1
kimpangya <nam74415133@gmail.com>
2024-02-20 01:17:53 +0900

    상품 폴더 설계

---
+++ src/main/java/com/sparta/myselectshop/entity/Folder.java
@@ -0,0 +1,32 @@
+package com.sparta.myselectshop.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "folder")
+public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    //원래 manytoone이면 즉시로딩임
+    //벗 계속필요한거 아니니까 지연로딩으로해~
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Folder(String name, User user) {
+        this.name = name;
+        this.user = user;
+    }
+}
